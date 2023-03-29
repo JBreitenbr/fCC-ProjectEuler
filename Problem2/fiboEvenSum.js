f2e076d17d65a166686fa2f05588ae823a30766b
JBreitenbr <119942939+JBreitenbr@users.noreply.github.com>
2023-03-29 20:07:53 +0200

    Create fiboEvenSum.js

---
+++ Problem2/fiboEvenSum.js
@@ -0,0 +1,16 @@
+function fibo(n) {
+    if (n < 2){
+        return 1
+    }else{
+        return fibo(n-2) + fibo(n-1);
+    }
+}
+
+function fiboEvenSum(n) {
+let sVar=0;
+  for(let i=1; i < 33; i++){
+      if(fibo(i)<=n && !(fibo(i)%2))
+          sVar+=fibo(i);
+            }
+              return sVar;
+}
