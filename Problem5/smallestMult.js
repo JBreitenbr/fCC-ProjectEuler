1c7edcb209aa9e9cde5df22040bef99eb3854b26
JBreitenbr <119942939+JBreitenbr@users.noreply.github.com>
2023-03-29 23:53:29 +0200

    Create smallestMult.js

---
+++ Problem5/smallestMult.js
@@ -0,0 +1,23 @@
+
+function gcd(a,b) {
+  return (b==0)?a:gcd(b,a%b);
+  }
+
+function scm(a,b){
+   return (a*b)/gcd(a,b);
+   }
+
+function scm_more(arr) {
+    return arr.reduce(scm)
+    }
+
+function smallestMult(num){
+     let hArr=[];
+     for(let i=1;i<=num;i++)
+        {
+          hArr.push(i);
+         }
+     return scm_more(hArr);
+     }
+
+smallestMult(20);
